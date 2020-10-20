b2d2ec31f91fb3f2d4ad5800e7843835cefb31a2
shenzhouyh <286817442@qq.com>
2020-10-21 00:05:55 +0800

    添加common中的统一异常处理

---
+++ shenzhouyh-ad-service/ad-common/src/main/java/com/shenzhouhyh/ad/advice/GlobalExceptionAdvice.java
@@ -0,0 +1,24 @@
+package com.shenzhouhyh.ad.advice;
+
+import com.shenzhouhyh.ad.exception.AdException;
+import com.shenzhouhyh.ad.vo.CommonResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by Qinyi.
+ */
+@RestControllerAdvice
+public class GlobalExceptionAdvice {
+
+    @ExceptionHandler(value = AdException.class)
+    public CommonResponse<String> handlerAdException(HttpServletRequest req,
+                                                     AdException ex) {
+        CommonResponse<String> response = new CommonResponse<>(-1,
+                "business error");
+        response.setData(ex.getMessage());
+        return response;
+    }
+}
---
+++ shenzhouyh-ad-service/ad-common/src/main/java/com/shenzhouhyh/ad/exception/AdException.java
@@ -0,0 +1,11 @@
+package com.shenzhouhyh.ad.exception;
+
+/**
+ * Created by Qinyi.
+ */
+public class AdException extends Exception {
+
+    public AdException(String message) {
+        super(message);
+    }
+}
