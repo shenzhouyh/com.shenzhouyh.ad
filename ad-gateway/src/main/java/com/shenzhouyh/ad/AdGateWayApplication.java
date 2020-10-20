b6f45ec44213fbffb8d32db3c92930e6abed3631
shenzhouyh <286817442@qq.com>
2020-10-20 23:24:53 +0800

    骨架初始化

---
+++ ad-gateway/src/main/java/com/shenzhouyh/ad/AdGateWayApplication.java
@@ -0,0 +1,15 @@
+package com.shenzhouyh.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description :
+ * @since : 10.7.0
+ */
+@SpringBootApplication
+public class AdGateWayApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(AdGateWayApplication.class,args);
+  }
+}
