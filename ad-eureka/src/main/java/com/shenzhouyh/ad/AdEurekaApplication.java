71f38445a72121cdff28fda1ec81b13ab23c89b2
shenzhouyh <286817442@qq.com>
2020-10-20 23:09:11 +0800

    Initial commit

---
+++ ad-eureka/src/main/java/com/shenzhouyh/ad/AdEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.shenzhouyh.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description :
+ * @since : 10.7.0
+ */
+@SpringBootApplication
+public class AdEurekaApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(AdEurekaApplication.class,args);
+  }
+}
